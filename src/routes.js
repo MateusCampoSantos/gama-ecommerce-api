d70b88259f663a2e314eb4536283bf2046b0dabd
fej0se <80367187+fej0se@users.noreply.github.com>
2021-06-03 12:50:47 -0300

    retirando swagger

--- src/routes.js
+++ src/routes.js
@@ -9,63 +9,14 @@ const departamentosController = new DepartamentosController();
 routes.get('/', (req, res) => {
   res.status(200).send({
     title: 'gama-ecommerce-api',
-    grupo: 'grupo 1',
+    grupo: 1,
     integrantes: 'Felipe José, Lucas Militante e Mateus Campos'
   })
 })
 
 
 routes.get('/produtos', produtosController.getAll)
-
 routes.get('/produto/:id', produtosController.getOne)
-
-
-/**
- * POST /produto
- * @summary Rota para pegar todos os produtos cadastrados.
- * @param {Produto} body.object.required - Objeto de produto
- * @example request - produto ok
- * 
- * {
-    "codProduto": 63254,
-    "descricao": "CONTROLE XBOX SERIES X",
-    "preco": 79,
-    "qtdEstoque": 10,
-    "disponivel": "sim",
-    "emDestaque": "sim",
-    "departamento": 8
-  }
- *
- * @example request - produto erro preço zerado
- * 
- * {
-    "codProduto": 8524,
-    "descricao": "MONITOR 21 POLEGADAS",
-    "preco": 0,
-    "qtdEstoque": 10,
-    "disponivel": "sim",
-    "emDestaque": "sim",
-    "departamento": 1
-  }
- * 
- * 
- * @return {object} 200 - response ok
- * @example response - 200 - exemplo response 200
- * {
-    "codProduto": 25754,
-    "descricao": "ADAPTADOR BLUETOOH USB RECEPTOR DE AUDIO P2",
-    "preco": 5,
-    "qtdEstoque": 10,
-    "disponivel": "sim",
-    "emDestaque": "sim",
-    "departamento": 1
-  }
- * @return {object} 400 - error response
- * @example response - 400 - exemplo erro 400
- * {
- *    "message": "produto já existe ou estrutura/informações inválidas, consulte a documentação"
- * }
- */
 routes.post('/produto', produtosController.cria)
 routes.put('/produto/:id', produtosController.atualiza)
 routes.get('/departamentos', departamentosController.getAll)
@@ -73,21 +24,3 @@ routes.get('/departamento/:id', departamentosController.getProdutos)
 
 
 export default routes;
-
-//swagger schemas
-/**
-*
-* @typedef {object} Produto
-* @property {number} codProduto.required - Código do produto
-* @property {string} descricao.required - Nome do produto
-* @property {number} preco.required - Preço do produto
-* @property {number} qtdEstoque.required - Quantidade em estoque do produto
-* @property {string} disponivel.required - Produto disponivel "sim"/"nao"
-* @property {string} emDestaque.required - Produto em destaque na loja "sim"/"nao"
-* @property {number} departamento.required - ID do departamento do produto
-*/
-
-/**
-* @typedef {object} ErroMessage
-* @property {string} message.required - mensagem de erro
-* */
--- src/server.js
+++ src/server.js
@@ -3,40 +3,6 @@ import routes from './routes';
 const app = express();
 const port = 3333; 
 
-//swagger
-import expressJSDocSwagger from 'express-jsdoc-swagger';
-
-//swagger
-const options = {
-  info: {
-    version: '1.0.0',
-    title: 'Gama ecommerce api',
-    description: '',  
-    license: {
-      name: 'MIT',
-    },
-  },
-  baseDir: __dirname,
-  // Glob pattern to find your jsdoc files (multiple patterns can be added in an array)
-  filesPattern: './**/*.js',
-  // URL where SwaggerUI will be rendered
-  swaggerUIPath: '/api-docs',
-  // Expose OpenAPI UI
-  exposeSwaggerUI: true,
-  // Expose Open API JSON Docs documentation in `apiDocsPath` path.
-  exposeApiDocs: false,
-  // Open API JSON Docs endpoint.
-  apiDocsPath: '/v3/api-docs',
-  // Set non-required fields as nullable by default
-  notRequiredAsNullable: false,
-  // You can customize your UI options.
-  // you can extend swagger-ui-express config. You can checkout an example of this
-  // in the `example/configuration/swaggerOptions.js`
-  swaggerUiOptions: {},
-};
-
-expressJSDocSwagger(app)(options);
-
 app.use(express.json());
 
 app.use(routes);
