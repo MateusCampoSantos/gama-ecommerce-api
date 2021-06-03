34e17a9d5a7ba95bf6ab59d2608644e355ce7334
Mateus Campos <mateuscamposantos@gmail.com>
2021-06-03 20:01:26 -0300

    Update checaProduto.js
    Mudança do local de definição da variável (verificarPreco; verificarDescricao)

--- src/utils/checaProduto.js
+++ src/utils/checaProduto.js
@@ -1,9 +1,10 @@
 export const checaProduto = (produto) =>{
   const verificarProduto = (produto.hasOwnProperty('codProduto') && produto.hasOwnProperty('descricao') && produto.hasOwnProperty('preco') && produto.hasOwnProperty('qtdEstoque') && produto.hasOwnProperty('disponivel') && produto.hasOwnProperty('emDestaque') && produto.hasOwnProperty('departamento'))
-  const verificarPreco = produto.preco <= 0
-  const verificarDescricao = ((produto.descricao.length < 3) && (typeof produto.descricao !== "string"))
 
   if(verificarProduto){
+    const verificarPreco = produto.preco <= 0
+    const verificarDescricao = ((produto.descricao.length < 3) && (typeof produto.descricao !== "string"))
+    
       if(verificarPreco){
           return false;
       }
