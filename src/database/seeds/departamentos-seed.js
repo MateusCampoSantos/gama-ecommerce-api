bf6f05dabd812b68aba2bb22c37d05d958292338
Mateus Campos <mateuscamposantos@gmail.com>
2021-06-10 09:06:49 -0300

    Add seed departamento

--- src/database/seeds/departamentos-seed.js
+++ src/database/seeds/departamentos-seed.js
@@ -5,9 +5,15 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('departamentos').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {idDepto: 1, nomeDepto: 'Adaptadores'},
+        {idDepto: 2, nomeDepto: 'Ferramentas'},
+        {idDepto: 3, nomeDepto: 'Eletronicos'},
+        {idDepto: 4, nomeDepto: 'Casa'},
+        {idDepto: 5, nomeDepto: 'Acessorios'},
+        {idDepto: 6, nomeDepto: 'Moveis'},
+        {idDepto: 7, nomeDepto: 'Tablets e Celulares'},
+        {idDepto: 8, nomeDepto: 'Games'},
+        {idDepto: 9, nomeDepto: 'Informatica'},
       ]);
     });
 };
